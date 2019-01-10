7bcaa8d314ce45b564baccdc30d9edd04bc4c4cb
isura <isura@wso2.com>
2019-01-10 10:55:48 +0530

    Fix https://github.com/wso2/product-is/issues/4265

--- components/event-handler-notification/org.wso2.carbon.identity.event.handler.notification/src/main/java/org/wso2/carbon/identity/event/handler/notification/DefaultNotificationHandler.java
+++ components/event-handler-notification/org.wso2.carbon.identity.event.handler.notification/src/main/java/org/wso2/carbon/identity/event/handler/notification/DefaultNotificationHandler.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.wso2.carbon.identity.event.handler.notification;
 
 import org.apache.commons.lang.StringUtils;
@@ -31,11 +48,11 @@ import java.util.Set;
  *
  */
 public class DefaultNotificationHandler extends AbstractEventHandler {
+
     private static final Log log = LogFactory.getLog(DefaultNotificationHandler.class);
 
     private static final String STREAM_DEFINITION_ID = "stream" ;
     private static final String NOTIFICATION_TEMPLATE_TYPE = "notification_template" ;
-
     private static final String DEFAULT_STREAM_ID = "id_gov_notify_stream:1.0.0";
 
     @Override
