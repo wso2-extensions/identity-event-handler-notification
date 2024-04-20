73328f2534680a8b5a8f0e6a27f3814c17b0f831
AnuradhaSK <anuradha199528@gmail.com>
2024-04-20 17:36:03 +0530

    resolve email templates and notification templates from root org

--- components/email-mgt/org.wso2.carbon.email.mgt/src/main/java/org/wso2/carbon/email/mgt/internal/I18nMgtDataHolder.java
+++ components/email-mgt/org.wso2.carbon.email.mgt/src/main/java/org/wso2/carbon/email/mgt/internal/I18nMgtDataHolder.java
@@ -1,23 +1,27 @@
 /*
- * Copyright (c) 2016, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2016-2024, WSO2 LLC. (http://www.wso2.com).
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
  * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 package org.wso2.carbon.email.mgt.internal;
 
 import org.wso2.carbon.identity.core.persistence.registry.RegistryResourceMgtService;
 import org.wso2.carbon.identity.governance.model.NotificationTemplate;
+import org.wso2.carbon.identity.organization.management.application.OrgApplicationManager;
+import org.wso2.carbon.identity.organization.management.service.OrganizationManager;
 import org.wso2.carbon.registry.core.service.RegistryService;
 import org.wso2.carbon.user.core.service.RealmService;
 
@@ -28,6 +32,8 @@ public class I18nMgtDataHolder{
     private RealmService realmService;
     private RegistryService registryService;
     private RegistryResourceMgtService registryResourceMgtService;
+    private OrganizationManager organizationManager;
+    private OrgApplicationManager sharedAppManager;
     private List<NotificationTemplate> defaultEmailTemplates = new ArrayList<>();
     private List<NotificationTemplate> defaultSMSTemplates = new ArrayList<>();
 
@@ -92,5 +98,45 @@ public class I18nMgtDataHolder{
     public List<NotificationTemplate> getDefaultSMSTemplates() {
 
         return defaultSMSTemplates;
+    }
+
+    /**
+     * Set the organization manager.
+     *
+     * @param organizationManager Organization manager.
+     */
+    public void setOrganizationManager(OrganizationManager organizationManager) {
+
+        this.organizationManager = organizationManager;
+    }
+
+    /**
+     * Get the organization manager.
+     *
+     * @return Organization manager.
+     */
+    public OrganizationManager getOrganizationManager() {
+
+        return organizationManager;
+    }
+
+    /**
+     * Get the shared application manager.
+     *
+     * @return Shared application manager.
+     */
+    public OrgApplicationManager getSharedAppManager() {
+
+        return sharedAppManager;
+    }
+
+    /**
+     * Set the shared application manager.
+     *
+     * @param sharedAppManager Shared application manager.
+     */
+    public void setSharedAppManager(OrgApplicationManager sharedAppManager) {
+
+        this.sharedAppManager = sharedAppManager;
     }
 }
