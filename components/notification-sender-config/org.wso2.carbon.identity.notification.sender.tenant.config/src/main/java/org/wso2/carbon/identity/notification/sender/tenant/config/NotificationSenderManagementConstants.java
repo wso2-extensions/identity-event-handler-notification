263d4ff4ea613739e43547a5d4c263babab1a1cc
Lashini Jayasekara <30428591+lashinijay@users.noreply.github.com>
2025-04-01 13:19:53 +0530

    Update EmailSenderDTO to support authType attribute (#315)
    * Update EmailSenderDTO to support authType

--- components/notification-sender-config/org.wso2.carbon.identity.notification.sender.tenant.config/src/main/java/org/wso2/carbon/identity/notification/sender/tenant/config/NotificationSenderManagementConstants.java
+++ components/notification-sender-config/org.wso2.carbon.identity.notification.sender.tenant.config/src/main/java/org/wso2/carbon/identity/notification/sender/tenant/config/NotificationSenderManagementConstants.java
@@ -46,14 +46,28 @@ public class NotificationSenderManagementConstants {
     public static final String PLUS = "+";
     public static final String URL_ENCODED_SPACE = "%20";
     public static final String CONFIG_MGT_ERROR_CODE_DELIMITER = "_";
+    public static final String SECRET_PROPERTIES = "_SECRET_PROPERTIES";
+
+    // Notification Type
+    public static final String EMAIL_PROVIDER = "EMAIL_PROVIDER";
+    public static final String SMS_PROVIDER = "SMS_PROVIDER";
+    public static final String PUSH_PROVIDER = "PUSH_PROVIDER";
 
     // Email Sender's main properties.
     public static final String SMTP_SERVER_HOST = "smtpServerHost";
     public static final String SMTP_PORT = "smtpPort";
     public static final String FROM_ADDRESS = "fromAddress";
+    public static final String CLIENT_ID = "clientId";
+    public static final String CLIENT_SECRET = "clientSecret";
+    public static final String TOKEN_ENDPOINT = "tokenEndpoint";
+    public static final String SCOPES = "scopes";
     public static final String USERNAME = "userName";
     public static final String PASSWORD = "password";
     public static final String EMAIL_PUBLISHER_TYPE = "email";
+
+    // Email Sender Authentication types.
+    public static final String BASIC = "BASIC";
+    public static final String CLIENT_CREDENTIAL = "CLIENT_CREDENTIAL";
 
     // SMS Sender's main properties.
     public static final String PROVIDER = "provider";
